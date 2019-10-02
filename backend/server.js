d042f64b8f918bbc453514cf80a7ad2e3490d94b
Olaide-EO <30773360+Olaide-EO@users.noreply.github.com>
2019-10-02 16:36:26 +0100

    connect route to server

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,13 @@ connection.once('open', () => {
     console.log('MongoDB database connection established successfully');
 })
 
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
+
+
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`)
 });
