d82ed719d7011a677bb767a1946a6e5c2c539437
Olaide-EO <30773360+Olaide-EO@users.noreply.github.com>
2019-10-03 14:45:20 +0100

    add routes to query database

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -24,4 +24,31 @@ router.route('/add').post((req, res) => {
                .catch(err => res.status(400).json('Error: ' + err));
 });
 
+router.route('/:id').get((req, res)=>{
+    Exercise.findById(req.params.id)
+            .then(exercise => res.json(exercise))
+            .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').delete((req, res)=>{
+    Exercise.findByIdAndDelete(req.params.id)
+            .then(exercise => res.json('Exercise Deleted.'))
+            .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/update/:id').post((req, res)=>{
+    Exercise.findById(req.params.id)
+            .then(exercise => {
+                exercise.username = req.body.username;
+                exercise.description = req.body.description;
+                exercise.duration = Number(req.body.duration);
+                exercise.date = Date.parse(req.body.date);
+                exercise.save()
+                        .then(() => res.json('Exercise updated!'))
+                        .catch(err => res.status(400).json('Error: ' + err));
+            })
+            .catch(err => res.status(400).json('Error: ' + err));
+});
+
+
 module.exports = router;
