2c938396862ba4f387e4bf989a6cbf2faaa37de9
Olaide-EO <30773360+Olaide-EO@users.noreply.github.com>
2019-10-03 23:37:10 +0100

    bind functions to this in super

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -3,6 +3,13 @@ import React, { Component } from 'react';
 export default class CreateExercises extends Component {
     constructor(props){
         super(props);
+    
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onChangeDescription = this.onChangeDescription.bind(this);
+        this.onChangeDuration = this.onChangeDuration.bind(this);
+        this.onChangeDate = this.onChangeDate.bind(this);
+
+
         this.state = {
             username: '',
             description: '',
